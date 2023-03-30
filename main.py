37632ade4000f187b9098c7fe20195d4dc6471d2
bahati <bulozealfani@gmail.com>
2023-03-29 23:06:08 -0500

    set initial capital, define indicator

--- main.py
+++ main.py
@@ -13,3 +13,19 @@ stocks = ['AAPL', 'TSLA', 'NFLX', 'UNH', 'JNJ', 'NVO', 'PFE', 'ABT', 'CVS', 'DIS
           'MSFT', 'GOOG', 'GOOGL', 'AMZN', 'NVDA', 'TSM', 'AVGO', 'ORCL', 'ADBE', 'AMD', 'QCOM', 'IBM', 'SONY', 'SPGI']
 cryptos = ['BTC', 'ETH', 'LTC', 'BCH', 'SOL', 'ETC', 'MATIC', 'DOGE', 'SHIB', 'ADA', 'UNI', 'LINK', 'COMP', 'AAVE', 'XTZ']
 interval = 'day'
+
+# Set the initial capital and risk management parameters
+capital = 100
+risk_per_trade = 0.02
+stop_loss_pct = 0.03
+
+# Define the indicators to use for the strategy
+bb_period = 20
+rsi_period = 14
+macd_fast_period = 12
+macd_slow_period = 26
+macd_signal_period = 9
+
+# Calculate the maximum amount of capital to risk per trade
+risk_amount = capital * risk_per_trade
+max_position_size = risk_amount / stop_loss_pct
